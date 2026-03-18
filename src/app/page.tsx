3f2ccaea08f22d39ae5da60653a98ab23320da66
Romas <r2udis@gmail.com>
2026-03-18 02:22:10 +0200

    Add Sentry and start parallel routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { getMyImages } from "~/server/queries";
 import Image from "next/image";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -21,13 +22,15 @@ export default async function HomePage() {
     <main className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex w-48 flex-col">
-          <Image
-            src={image.url}
-            alt={image.name}
-            width={500}
-            height={500}
-            style={{ objectFit: "contain" }}
-          />
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              alt={image.name}
+              width={500}
+              height={500}
+              style={{ objectFit: "contain" }}
+            />
+          </Link>
           <div>{image.name}</div>
         </div>
       ))}
